93563b06b1076d68f0e183f42d471ec092d060b1
NeroXie <xyh30902@163.com>
2020-04-21 17:07:29 +0800

    增加题解

--- algo/src/algorithms/LeetCode/371_两整数之和.js
+++ algo/src/algorithms/LeetCode/371_两整数之和.js
@@ -1,10 +1,33 @@
-
+/**
+ * 
+ * 题目地址：https://leetcode-cn.com/problems/sum-of-two-integers/
+ * 
+ * 解题思路：
+ * 
+ * 加法可以通过&运算和^运算实现：
+ * 
+ * 1. 两个二进制数相加，^运算的结果是不考虑进位时的结果
+ * 2. 两个二进制数相加，&运算的结果是对应为是否有进位，如果结果出现1的话，则左移（<<）1位后和^运算的结果继续做加法，直到&运算的结果为0，此时&运算的结果就是两个数的和
+ * 
+ * 时间复杂度：
+ * 
+ * 空间复杂度：
+ */
 const lc_getSum = (a, b) => {
     return add(a, b)
 }
 
 const add = (a, b) => {
-    return b === 0 ? a : add(a ^ b, (a & b) << 1);
+    // 递归
+    return b === 0 ? a : add(a ^ b, (a & b) << 1)
+    // // 迭代
+    // while(b) {
+    //     const temp = a ^ b
+    //     b = (a & b) << 1
+    //     a = temp
+    // }
+
+    // return a
 }
 
 export const lc_getSumTest = () => {
--- algo/src/algorithms/LeetCode/389_找不同.js
+++ algo/src/algorithms/LeetCode/389_找不同.js
@@ -1,4 +1,29 @@
 
+/**
+ * 题目地址：https://leetcode-cn.com/problems/find-the-difference/
+ * 
+ * 解题思路：使用异或运算
+ * 
+ * 异或运算关系：
+ * 1. a⊕0 = a
+ * 2. a⊕a = 0
+ * 3. a⊕b = c，这个c是什么我们不需要关心，但只要记得 c⊕a=b, c⊕b=a就可以
+ * 
+ * 以s="abcd", t= "eabcd"为例，
+ * 我不需要知道 a^b^c^d的结果是什么，只要知道a^b^c^d^a = b^c^d。以此类推
+ * a^b^c^d^e^a^b^c^d^ = e
+ * 
+ * js中字符不能直接异或运算，需要转成对应的ascii码
+ * 
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(1)
+ */
+
+/**
+ * @param {string} s 
+ * @param {string} t 
+ * @return {string}
+ */
 const lc_findTheDifference = (s, t) => {
     let res = t.charCodeAt(t.length - 1)
     for (let i = 0; i < s.length; i++) {
