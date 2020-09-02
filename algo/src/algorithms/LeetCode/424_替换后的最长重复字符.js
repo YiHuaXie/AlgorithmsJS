377648531afcfdb4da00ac61ba89a0872725618f
NeroXie <xyh30902@163.com>
2020-09-02 20:33:47 +0800

    code update

---
+++ algo/src/algorithms/LeetCode/424_替换后的最长重复字符.js
@@ -0,0 +1,16 @@
+/**
+ * 题目地址：https://leetcode-cn.com/problems/longest-repeating-character-replacement/
+ * 
+ * 解题思路：滑动窗口
+ * 
+ * 时间复杂度：
+ * 空间复杂度：
+ */
+const lc_characterReplacement = (s, k) => {
+
+};
+
+export const lc_characterReplacementTest = () => {
+    console.log(lc_characterReplacement("ABAB", 2));
+    console.log(lc_characterReplacement("AABABBA", 1));
+};
